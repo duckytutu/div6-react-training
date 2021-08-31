34c929dea09a295d23cbd104e6d91733a4ff88d4
Tung Nguyen <tungnt3@smartosc.com>
2021-08-31 19:27:31 +0700

    render list and data binding sample

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ function App() {
   const [numberA, setNumberA] = useState(1);
   const [numberB, setNumberB] = useState(2);
   const [show, setShow] = useState(true);
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Do task 1', done: false},
+    { id: 2, title: 'Do task 2', done: true},
+    { id: 3, title: 'Do task 3', done: false},
+  ])
+  const [newTodo, setNewTodo] = useState({ id: "", title: "New todo", done: false})
 
   useEffect(() => {
-    alert('Page loaded');
+    // alert('Page loaded');
   }, []) // Array dependencies (điều kiện để chạy lại logic trong useEffect)
 
   const total = useMemo(() => {
@@ -26,6 +32,16 @@ function App() {
     setShow(!show);
   }
 
+
+  const onTitleChange = (event) => {
+    // Spread operator doc: https://viblo.asia/p/su-dung-spread-operator-trong-javascript-gDVK24welLj
+    setNewTodo({...newTodo, title: event.target.value});
+  }
+
+  const addTodo = () => {
+    setTodos([...todos, newTodo])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -35,8 +51,17 @@ function App() {
         <span>{ numberB }</span>
         <span>{ c }</span>
         <span>{ total }</span>
-
         <button onClick={() => test()}>Click me</button>
+
+        <ul>
+        {todos.map((item, index) => {
+          return <li key={`todo-${index}`}>{ item.title } - { item.done ? "Done" : "Not done" }</li>
+        })}
+        </ul>
+
+        <input type="text" value={newTodo.title} onChange={onTitleChange}/>
+        <button onClick={addTodo}>Add</button>
+
       </header>
     </div>
   );
